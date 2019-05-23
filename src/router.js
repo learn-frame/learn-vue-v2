19293e922db3efc4d19eab9ba25310d185ae146d
yanceyleo <developer@yanceyleo.com>
2019-05-23 18:39:27 +0800

    feat: Finish navagitor guard

--- src/router.js
+++ src/router.js
@@ -122,4 +122,18 @@ router.beforeEach((to, from, next) => {
   }
 });
 
+// 全局级守卫
+// beforeEach (全局前置守卫) 用于做路由拦截
+// beforeResolve (全局解析守卫) 组件内守卫和异步路由组件被解析之后
+// afterEach (全局后置守卫)
+
+// 组件级守卫
+// beforeRouteEnter
+// beforeRouteUpdate
+// beforeRouteLeave 这个离开守卫通常用来禁止用户在还未保存修改前突然离开，也就是在离开前显示一个确认框
+
+// React-Router-4 有一个 <BrowserRouter /> 组件，该组建有一个 getUserConfirmation 属性
+// 该属性和 beforeRouteLeave 作用类似
+// 不过它要在需要做确认的组件显式的添加一个 <Prompt message='' /> 组件
+
 export default router;
