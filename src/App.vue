9482b606126d751a910df68584dcdbb6da3fc0c6
Yancey Leo <yanceyofficial@gmail.com>
2020-06-24 14:26:53 +0800

    feat: 🎸 update appbar

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,30 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
+    <v-app-bar app color="black" dark dense>
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <router-link to="/">
+          <v-img
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            src="./assets/logo.png"
+            transition="scale-transition"
+            width="40"
+          />
+        </router-link>
       </div>
 
       <v-spacer></v-spacer>
 
-      <router-link to="/about">
-        <v-btn text>
-          About
-        </v-btn>
-      </router-link>
-      <router-link to="/basic">
+      <router-link to="/basic" class="nav-link">
         <v-btn text>
           Basic
+        </v-btn>
+      </router-link>
+
+      <router-link to="/vuex" class="nav-link">
+        <v-btn text>
+          Vuex
         </v-btn>
       </router-link>
     </v-app-bar>
@@ -47,11 +41,16 @@ import Vue from 'vue'
 export default Vue.extend({
   name: 'App',
 
-  components: {
-  },
+  components: {},
 
   data: () => ({
     //
   }),
 })
 </script>
+
+<style lang="scss" scoped>
+.nav-link {
+  text-decoration: none;
+}
+</style>
