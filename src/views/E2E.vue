359c31a94f42d0ea28871b4f9d00dec3bf9ce4b9
Yancey Leo <yanceyofficial@gmail.com>
2020-07-30 14:56:07 +0800

    feat: support e2e

--- src/views/E2E.vue
+++ src/views/E2E.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="e2e">
     <h1>Learn E2E</h1>
+    <v-btn depressed color="primary" @click="handleClick">click me!</v-btn>
+    <p v-if="showLog">Hello, world.</p>
+
+    <p>https://www.baidu.com</p>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'E2E',
+
+  mounted() {
+    // TODO:
+  },
+
+  data: () => ({
+    showLog: false,
+  }),
+
+  methods: {
+    handleClick() {
+      this.showLog = true
+    },
+  },
+})
+</script>
--- tests/e2e/specs/e2e.spec.js
+++ tests/e2e/specs/e2e.spec.js
@@ -1,12 +1,21 @@
-describe('Learn E2E', () => {
+describe('查找页面是否存在某个 HTML 标签', () => {
   it('Check H1 tag', () => {
     cy.visit('/e2e')
     cy.contains('h1', 'Learn E2E')
+    cy.contains('E2E')
   })
 })
 
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
+describe('断言测试', () => {
+  it('url 是否包含 `e2e`', () => {
+    cy.url().should('include', 'e2e')
+  })
+})
+
+describe('交互测试', () => {
+  it('clicks the button!', () => {
+    // cy.contains('p', 'Hello, world.')
+    cy.contains('click me!').click()
+    cy.contains('p', 'Hello, world.')
   })
 })
