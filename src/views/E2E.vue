583531c48ec3abba14797b4125f164fea4a285a0
YanceyOfficial <yanceyofficial@gmail.com>
2021-05-18 17:31:59 +0800

    feat: update

--- src/views/E2E.vue
+++ src/views/E2E.vue
@@ -1,35 +1,37 @@
 <template>
-  <div class="e2e">
-    <h1>Learn E2E</h1>
+    <div class="e2e">
+        <h1>Learn E2E</h1>
 
-    <v-btn
-      depressed
-      color="primary"
-      @click="handleClick"
-      :class="{ 'is-fetching': showLog }"
-    >click me!</v-btn>
-    <p v-if="showLog" class="log">Hello, world.</p>
+        <v-btn
+            depressed
+            color="primary"
+            @click="handleClick($event)"
+            :class="{ 'is-fetching': showLog }"
+            >click me!</v-btn
+        >
+        <p v-if="showLog" class="log">Hello, world.</p>
 
-    <p class="c-image">https://www.baidu.com</p>
+        <p class="c-image">https://www.baidu.com</p>
 
-    <input type="text" class="c-input" />
-  </div>
+        <input type="text" class="c-input" />
+    </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'E2E',
+    name: 'E2E',
 
-  data: () => ({
-    showLog: false,
-  }),
+    data: () => ({
+        showLog: false,
+    }),
 
-  methods: {
-    handleClick() {
-      this.showLog = true
+    methods: {
+        handleClick(e: Event) {
+            e.preventDefault()
+            this.showLog = true
+        },
     },
-  },
 })
 </script>
